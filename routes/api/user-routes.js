29626c55ddb2c75fee1632239fcea1f477ab360f
Zak Stamps <theoneandonlyzako@gmail.com>
2021-06-10 23:43:29 -0700

    added comment id

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models/Index');
 
 // get all users
 router.get('/', (req, res) => {
@@ -15,34 +15,32 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
+  attributes: { exclude: ['password'] },
+  where: {
+    id: req.params.id
+  },
+  include: [
+    {
+      model: Post,
+      attributes: ['id', 'title', 'post_url', 'created_at']
     },
-    include: [
-      {
+    // include the Comment model here:
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'created_at'],
+      include: {
         model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
-      },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'created_at'],
-        include: {
-          model: Post,
-          attributes: ['title']
-        }
-      },
-      {
-        model: Post,
-        attributes: ['title'],
-        through: {
-          model: Vote,
-          attributes: []
-        },
-
+        attributes: ['title']
       }
-    ]
-  })
+    },
+    {
+      model: Post,
+      attributes: ['title'],
+      through: Vote,
+      as: 'voted_posts'
+    }
+  ]
+})
     .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
